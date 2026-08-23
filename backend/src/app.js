f89aae242186b978c68962a155f6f52232b107f7
shubhamshedage-glitvh <shedages747@gmail.com>
2026-08-23 13:07:44 +0530

    Allow deployed frontend in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:[ 
+        "http://localhost:5173",
+        "https://spendwise-frontend-7l33.onrender.com/"
+    ],
     credentials: true
 }));
 
